b7669bd2118c404fdb165be6e58ce8a232a0bc53
Kriangkrai Prommaithong <d8kriangkrai@gmail.com>
2017-03-27 12:27:36 +0700

    fix layout

--- public/src/ctrl/navCtrl.js
+++ public/src/ctrl/navCtrl.js
@@ -13,11 +13,16 @@
         self.checkTutorNav
         self.checkAdminNav = false
         $firebaseAuth().$onAuthStateChanged(function(user) {
+            localStorageService.get("status") == "school" ? $scope.school = true : $scope.school = false
+            localStorageService.get("status") == "tutor" ? $scope.tutor = true : $scope.tutor = false
+            localStorageService.get("status") == "student" ? $scope.student = true : $scope.student = false
             user != null ? $scope.hide = true : $scope.hide = false
             localStorageService.get("status") == "school" ? self.checkSchoolNav = true : self.checkSchoolNav = false
             localStorageService.get("status") == "student" ? self.checkStdNav = true : self.checkStdNav = false
             localStorageService.get("status") == "tutor" ? self.checkTutorNav = true : self.checkTutorNav = false
-            if (localStorageService.get("checkAdmin") == undefined) {} else if (localStorageService.get("checkAdmin").email == "admin@system") {
+            if (localStorageService.get("checkAdmin") == undefined) {
+                console.log("hello user");
+            } else if (localStorageService.get("checkAdmin").email == "admin@system") {
                 self.checkAdminNav = true
                 $scope.hide = true
             }
